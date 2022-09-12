c0afddcb052d1b9dd9a311e265c7a3a6e2251fdb
Frankie Xiao <fxiaoX089@live.com>
2022-09-12 17:57:58 -0500

    changed index.html and style.css

---
+++ script.js
@@ -0,0 +1,15 @@
+const displayController = () => {
+    // updates display when player marks a cell
+    
+    // reset board when game over or click reset button
+}
+
+const gameBoard = () => {
+    // tracks turns
+
+    // detects for winner, loser, and draw
+}
+
+const Player = (name) => {
+
+}
