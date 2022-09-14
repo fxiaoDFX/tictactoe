5809808ae31b88174197ae82785524c93151e15b
Frankie Xiao <fxiaoX089@live.com>
2022-09-14 11:07:03 -0500

    add functions to add mark and restart game

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
-const displayController = () => {
-    // updates display when player marks a cell
-    
+const gameController = (() => {
+    const cells = document.querySelectorAll('[data-cell]');
+    let xTurn = true;
+    const x = 'x';
+    const circle = 'circle';
+
+    // start game
+    circleTurn = false;
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once : true});
+    });
+
+    function handleClick(e) {
+        console.log('clicked');
+
+        const cell = e.target;
+        const currentMark = xTurn ? x : circle;
+        // placeMark
+        displayController.placeMark(cell, currentMark);
+        // check for win
+        // check for draw
+        // switch turns
+    }
+
+    /**
+     * Get list of cells
+     * @return {NodeList} a list of cells
+     */
+    function getCells() {
+        return cells;
+    }
+
+    return {getCells};
+})();
+
+const displayController = (() => {
+    // place mark of current player when cell is clicked
+    const placeMark = (cell, currentMark) => {
+        cell.classList.add(currentMark);
+    }
     // reset board when game over or click reset button
-}
+    const restartButton = document.getElementById('restartButton');
+    restartButton.addEventListener('click', resetGame);
 
-const gameBoard = () => {
-    // tracks turns
+    /**
+     * Hide winner screen and clear the board
+     */
+    function resetGame() {
+        const displayWinnerScreen = document.getElementById('displayWinner');
+        displayWinnerScreen.classList.remove('show');
 
-    // detects for winner, loser, and draw
-}
+        const cells = gameController.getCells();
+        removeMark(cells);
+    }
 
-const Player = (name) => {
+    /**
+     * Remove all marks on the board
+     * @param {NodeList} list of cells
+     */
+    function removeMark(list) {
+        list.forEach(cell => {
+            cell.className = 'cell';
+        })
+    }
 
-}
+
+    return {placeMark, resetGame};
+})();
